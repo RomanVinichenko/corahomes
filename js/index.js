bfe0696ac1494d0a202e4d2943ec9da38f3daddd
RomanVinichenko <chicagezzz@gmail.com>
2022-08-24 14:50:59 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-$(function () {});
+let header = document.querySelector(".header");
+
+window.addEventListener('scroll', function () {
+  let scrollPos = window.scrollY;
+
+  if (header && scrollPos > 10) {
+    header.classList.add("header--active");
+  } else {
+    header.classList.remove("header--active");
+  }
+});
