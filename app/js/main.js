785a3f4a3f85c8823f6e79308818f24675e5c131
RomanVinichenko <chicagezzz@gmail.com>
2022-08-30 01:55:42 +0300

    home1

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,20 @@ window.addEventListener('scroll', function () {
     header.classList.remove("header--active");
   }
 });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 16,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
